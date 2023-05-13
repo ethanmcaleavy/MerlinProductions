88464bd28a8e30520d867f96ed4f71b020b388fd
unknown <ethan.mcaleavy@gmail.com>
2023-05-12 18:43:28 -0600

    Server running, added poster spacing for front page

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # MerlinProductions
+To start the server on our localhost use the command "npm start".
+If this doesn't work then make sure you do "npm install express".
+If that doesn't work make sure you have nodejs installed from their website.
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const server = express();
+server.set('view engine', 'ejs');
+const port = 8080;
+
+
+server.use(express.static(__dirname + '/public'));
+
+server.get('/', (req, res) => {
+    res.render('pages/home')
+})
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
