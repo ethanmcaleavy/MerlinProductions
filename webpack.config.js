dabec920e7f55ed8b8238a5d03fd995086c95cb8
ethanmcaleavy <ethan.mcaleavy@gmail.com>
2024-06-29 16:10:06 -0600

    added more font support

--- webpack.config.js
+++ webpack.config.js
@@ -32,13 +32,15 @@ module.exports = {
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: 'fonts/',
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[contenthash].[ext]', // or another appropriate naming scheme
+              outputPath: 'fonts/',
+            },
           },
-        },
+        ],
       },
       {
         test: /\.(jpg|jpeg|png|gif)$/,
